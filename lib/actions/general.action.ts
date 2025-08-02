dff069c043bec4a4b192c14a1497e5964cff6a53
tallapallysrujana <tallapallysrujana2004@gmail.com>
2025-08-02 23:12:15 +0530

    show feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -49,7 +49,7 @@ export async function createFeedback(params: CreateFeedbackParams) {
             `- ${sentence.role}: ${sentence.content}\n`
         )).join('');
 
-        const { object } = await generateObject({
+        const { object : { totalScore, categoryScores, strengths, areasForImprovement, finalAssessment } } = await generateObject({
             model: google('gemini-2.0-flash-001', {
                 structuredOutputs: false,
             }),
@@ -70,23 +70,22 @@ export async function createFeedback(params: CreateFeedbackParams) {
                 "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
         });
 
-        const feedback = {
+        const feedback = await db.collection('feedback').add({
             interviewId,
             userId,
-            totalScore: object.totalScore,
-            categoryScores: object.categoryScores,
-            strengths: object.strengths,
-            areasForImprovement: object. areasForImprovement,
-            finalAssessment: object.finalAssessment,
+            totalScore,
+            categoryScores,
+            strengths,
+            areasForImprovement,
+            finalAssessment,
             createdAt: new Date().toISOString()
+        })
+        return {
+            success: true,
+            feedbackId: feedback.id
         }
-
-        const newFeedback = await db.collection('feedback')
-            .add(feedback)
-
-        return { success: true, feedbackId: newFeedback.id };
     } catch (e) {
-        console.log('Error saving feedback', e)
+        console.error('Error saving feedback', e)
 
         return { success: false }
     }
